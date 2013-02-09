6dcd7534e3642a746ff182ae6f96f7c637f66d28
Ryan Sonnek <ryan.sonnek@gmail.com>
2013-02-09 11:55:10 -0600

    update to use separate form

--- content.js
+++ content.js
@@ -1,11 +1,37 @@
 $(function() {
-  $(document).on('focus', 'textarea', function() {
-    console.log('here');
-    $(this).addClass('focaltyped');
+  var FOCALTYPE_TEMPLATE = '<div class="focaltype-overlay">'+
+    '<a href="#" class="close">close</a>'+
+    '<textarea></textarea>'+
+  '</div>';
+
+  var $document = $(document);
+  var $body = $(document.body);
+
+  var $focaltypeOverlay = $(FOCALTYPE_TEMPLATE).appendTo($body);
+  var $focaltypeTextarea = $focaltypeOverlay.find('textarea');
+  var $targetInput = null;
+
+  $document.on('focus', 'textarea', function() {
+    var $input = $(this);
+    debugger;
+    console.log($input);
+    if ($input.parent().hasClass('focaltype-overlay')) {
+      console.log('skip');
+      return;
+    }
+    console.log('focus');
+    $body.addClass('focaltyped');
+    $targetInput = $input
+    $focaltypeTextarea.val($input.val()).attr('placeholder', $input.attr('placeholder')).focus();
   });
 
-  $(document).on('blur', 'textarea.focaltyped', function() {
-    console.log('here');
-    $(this).removeClass('focaltyped');
+  $focaltypeTextarea.on('blur', function() {
+    console.log('blur');
+    $body.removeClass('focaltyped');
+    if ($targetInput) {
+      $targetInput.val($focaltypeTextarea.val()).focus();
+      $targetInput = null;
+      $focaltypeTextarea.val('').removeAttr('placeholder');
+    }
   });
 });
